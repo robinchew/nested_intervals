c88b328bdd93b0dc43dd32c739732472dfefb0f4
Robin Chew <robin@obsi.com.au>
2015-08-02 23:48:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ second_child = make_child_of(root, 2)
 print '2nd child of root:', second_child
 
 print '1st grand child', make_child_of(first_child, 1)
+print '2nd grand child', make_child_of(first_child, 2)
+print '3rd grand child', make_child_of(first_child, 3)
+
+print
+print '1st grand child of child 2', make_child_of(second_child, 1)
