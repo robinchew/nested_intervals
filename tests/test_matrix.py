a837012fa2be1795644d13940cd8d651127a211a
Robin <me@robin.com.au>
2015-11-06 17:00:26 +0800

    Test get_ancestors_matrix.

---
+++ tests/test_matrix.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from nested_intervals.matrix import ROOT_MATRIX, Matrix, get_ancestors_matrix
+
+
+class MatrixTest(TestCase):
+    def test_ancestors(self):
+        self.assertEqual(
+            get_ancestors_matrix(Matrix(7, -5, 10, -7)),
+            [Matrix(5, -3, 7, -4), Matrix(3, -1, 4, -1), ROOT_MATRIX])
