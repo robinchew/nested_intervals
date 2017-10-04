ad08a7a1ac94caadce0be37a3b5ae7fee49ad81a
Robin <me@robin.com.au>
2017-10-04 22:37:36 +0800

    Extra argument for 'register_fields' function so that an existing 'parent' field is allowed.

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from django.db import models as django_models
 def get_model_field_names(model_class):
     return tuple(field.name for field in model_class._meta.fields)
 
-def register_fields(model_class, *field_names):
+def register_fields(model_class, *field_names, **kwargs):
     assert len(field_names) == 5, 'First 4 names are for nested intervals integer fields. The 5th name is a parent field.'
     model_class._nested_intervals_field_names = field_names
 
@@ -24,9 +24,18 @@ def register_fields(model_class, *field_names):
     # Register parent field
 
     parent_field_name = field_names[-1]
-    django_models.ForeignKey(
-        'self',
-        related_name='children',
-        null=True,
-        blank=True,
-    ).contribute_to_class(model_class, parent_field_name)
+
+    if parent_field_name in get_model_field_names(model_class):
+        # If parent field already exists in Model, either
+        # raise exception or ignore.
+        if kwargs.get('ignore_existing_parent_field', False):
+            pass
+        else:
+            raise FieldError("'{}' is already an existing model field.".format(parent_field_name))
+    else:
+        django_models.ForeignKey(
+            'self',
+            related_name='children',
+            null=True,
+            blank=True,
+        ).contribute_to_class(model_class, parent_field_name)
